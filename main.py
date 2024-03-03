5c4b9053ec557cf994947b57524ea326be0dc438
Bhadvaheroku <156423343+Bhadvaheroku@users.noreply.github.com>
2024-03-03 14:02:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ ERROR_BUTTON = InlineKeyboardMarkup(
 
     
 
-@bot@bot.on_message(filters.command("start") & filters.private)
+@bot.on_message(filters.command("start") & filters.private)
 async def start(bot, message):
     user = message.from_user
     user_link = f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
@@ -50,6 +50,7 @@ async def start(bot, message):
            "๏ ⚔️ ᴘᴏᴡᴇʀ ʙʏ @DeathxBotz"
     image_url = "https://telegra.ph/file/58e635c527bbf872a512f.jpg"
     await message.reply_photo(photo=image_url, caption=text, reply_markup=INLINE_SELECT)
+    
 @bot.on_message(filters.photo & filters.private)
 async def photo_upload(bot, message):
     msg = await message.reply("Uploading", quote=True)
