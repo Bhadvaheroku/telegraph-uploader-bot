6f276778af93267d8adde3a089825cb69ec78722
Bhadvaheroku <156423343+Bhadvaheroku@users.noreply.github.com>
2024-03-03 11:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345678))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    ADMIN_ID = os.environ.get("ADMIN_ID")
